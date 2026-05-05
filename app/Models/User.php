630c37f217e71adb5f7f5c3a8de0d6336c447e22
emoajib-blip <emoajib@gmail.com>
2026-05-06 05:49:49 +0700

    chore(models): fix syntax in profilePicture accessor
    Add a missing comma in the debug logging array within the profilePicture attribute accessor.

--- app/Models/User.php
+++ app/Models/User.php
@@ -164,15 +164,15 @@ class User extends Authenticatable implements HasMedia
     public function profilePicture(): Attribute
     {
         return new Attribute(
-            get: fn ($value) => {
+            get: function ($value) {
                 $mediaUrl = $this->getFirstMediaUrl('avatar');
                 \Log::debug('Profile picture accessor called', [
                     'user_id' => $this->id,
                     'media_url' => $mediaUrl,
                     'has_media' => $this->hasMedia('avatar'),
-                    'identity_picture' => $this->identity->profile_picture ?? null
+                    'identity_picture' => $this->identity->profile_picture ?? null,
                 ]);
-                
+
                 return $mediaUrl ?: ($this->identity->profile_picture
                     ?? 'https://www.gravatar.com/avatar/'.md5(strtolower(trim($this->email))).'?s=128&d=identicon');
             },
